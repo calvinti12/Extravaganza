a6082035291e68ffa6a42210ee47c0f9017115ef
Michael Realy <mjrealy@gmail.com>
2017-06-28 21:07:43 -0500

    Finished app.js file

--- app/app.js
+++ app/app.js
@@ -1,3 +1,9 @@
 // Dependencies
 var React = require("react");
 var ReactDOM = require("react-dom");
+
+// Grab routes
+var routes = require("./config/routes");
+
+// Render contents
+ReactDOM.render(routes, document.getElementById("app"));
