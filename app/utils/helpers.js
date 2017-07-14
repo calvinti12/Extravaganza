0ff0c716ea7c5d825786df9bed69129ceca2240e
Caryn Carter <Caryn.carter@northwestern.edu>
2017-07-13 21:43:22 -0500

    geolocation now works and pushes to state for the user

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,6 +1,8 @@
 // Include the Axios library for HTTP requests
 var axios = require("axios");
 
+var path = require("path");
+
 //SeatGeek Api url and key
 var seatgeek_APIKey = {
   Url: 'https://api.seatgeek.com/2/',
@@ -49,7 +51,6 @@ var helpers = {
     return axios.get(url);
   } 
 
-
 };
 
 
@@ -58,7 +59,3 @@ var helpers = {
 // We export the helpers function
 module.exports = helpers;
 
-//        "performers.slug": artistName,
-        // "q": event,
-        // "venue": venue,
-        // "taxonomies": taxonomy
