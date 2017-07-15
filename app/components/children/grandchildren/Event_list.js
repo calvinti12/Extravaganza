0e9c1d85fcab72dd97023c1c1dff95658738d95d
Jared Moscrip <jaredmoscrip@gmail.com>
2017-07-15 13:57:43 -0500

    Markers working!

--- app/components/children/grandchildren/Event_list.js
+++ app/components/children/grandchildren/Event_list.js
@@ -14,14 +14,16 @@ var Event_list = React.createClass({
     componentDidUpdate: function() {
         console.log("Event map component has updated");
         locations = [];
-        console.log(this.state);
+
         //this.setState({ eventTimes: [] });
         if(this.props.results.events) {
             for (var i = 0; i < this.props.results.events.length; i++) {
+                var venueName = this.props.results.events[i].title;
                 // handles the venue geocoding to push into google maps API
                 var venueLat = this.props.results.events[i].venue.location.lat;
                 var venueLng = this.props.results.events[i].venue.location.lon;
                 var venueLocation = {}
+                venueLocation["name"] = venueName;
                 venueLocation["lat"] = venueLat;
                 venueLocation["lng"] = venueLng;
                 locations.push(venueLocation);
@@ -32,9 +34,12 @@ var Event_list = React.createClass({
                 // var uglyTime = uglyDateTime.slice(11, 19);
                 // console.log("ugly", uglyDate, uglyTime);
             }
-        }
+
         // initializes the map once the for loop has finished pulling geolocations from props
         this.initMap();
+
+        }
+        
     },
     handleClick: function(event) {
         console.log("CLICKED", event);
@@ -47,91 +52,34 @@ var Event_list = React.createClass({
     },
     initMap: function() {
 
-        var markers;
-        var map;
-        var markerCluster;
+        var labels = '1234567890';
+        var labelIndex = 0;
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 8,
-            center: {lat: 41.8781, lng: -87.9298}
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: new google.maps.LatLng(41.8781, -87.9298),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
-        // Create an array of alphabetical characters used to label the markers.
-        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var infowindow = new google.maps.InfoWindow();
 
-        // Add some markers to the map.
-        // Note: The code uses the JavaScript Array.prototype.map() method to
-        // create an array of markers based on a given "locations" array.
-        // The map() method here has nothing to do with the Google Maps API.
-        markers = locations.map(function(location, i) {
-            return new google.maps.Marker({
-                position: location,
-                label: labels[i % labels.length],
-                title: "Click to zoom!"
-            });
-        });
+        var marker, i;
 
-        // var beaches = [
-        //   ['Bondi Beach', -33.890542, 151.274856, 4],
-        //   ['Coogee Beach', -33.923036, 151.259052, 5],
-        //   ['Cronulla Beach', -34.028249, 151.157507, 3],
-        //   ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-        //   ['Maroubra Beach', -33.950198, 151.259302, 1]
-        // ];
+        for (i = 0; i < locations.length; i++) {  
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+            label: labels[labelIndex++ % labels.length],
+            animation: google.maps.Animation.DROP,
+            map: map
+          });
 
-        // for (var i = 0; i < locations.length; i++) {
-
-            var contentString = '<div id="content">'+
-              '<div id="siteNotice">'+
-              '</div>'+
-              '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-              '<div id="bodyContent">'+
-              '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-              'sandstone rock formation in the southern part of the '+
-              'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-              'south west of the nearest large town, Alice Springs; 450&#160;km '+
-              '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-              'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-              'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-              'Aboriginal people of the area. It has many springs, waterholes, '+
-              'rock caves and ancient paintings. Uluru is listed as a World '+
-              'Heritage Site.</p>'+
-              '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-              'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-              '(last visited June 22, 2009).</p>'+
-              '</div>'+
-              '</div>';
-
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-
-            // markers = locations.map(function(location, p) {
-            // return new google.maps.Marker({
-            //     position: location[i],
-            //     label: labels[p % labels.length],
-            //     title: "something small..."
-            // });
-
-            // markers = new google.maps.Marker({
-            //   position: location[i],
-            //   map: map,
-            //   title: "Something else...",
-            // });
-
-            google.maps.event.addListener(markers, 'click', function() {
-                console.log("Marker Clicked!!");
-                infowindow.open(map, markers);
-            });
-        // };
-        // marker = new google.maps.Marker({
-        //   position: {lat: 41.878114, lng: -87.629798},
-        //   map: map
-        // });
-
-        // Add a marker clusterer to manage the markers.
-        // markerCluster = new MarkerClusterer(map, marker,
-        //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(locations[i].name);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+        }
     
     },
     renderEvents: function() {
